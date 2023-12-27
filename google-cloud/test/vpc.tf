c1efdbc083b063a5ceff9022bf7edfc07c75a55e
Daniel <danielarturo.marquez@gmail.com>
2023-12-26 23:15:17 -0400

    Add vpc

---
+++ google-cloud/test/vpc.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+resource "google_compute_network" "vpc" {
+  name                    = "${var.gcp_project_id}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.gcp_project_id}-subnet"
+  region        = var.gcp_region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
